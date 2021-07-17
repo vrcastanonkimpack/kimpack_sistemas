5c2492e63f6e4d2e99cad8f392b4ababb6e8e9bc
victor rene <vrcastanonkmpack>
2021-07-17 02:24:41 -0500

    Primer prueba de ejecución de script

--- EjecutarScript.sql
+++ EjecutarScript.sql
@@ -1,6 +1,6 @@
 SET NOCOUNT ON
 GO
 :On Error exit
-:r C:\Users\Servidor\Documents\Rene\Scripts\DML_ActualizaStatusOC_V2.sql
+:r C:\Users\Servidor\Documents\Rene\Scripts\Kimpack\DML_ActualizaStatusOC_V2.sql
 PRINT 'Script Ejecutado correctamente'
 GO
