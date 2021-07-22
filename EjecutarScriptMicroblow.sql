e650e564e4caf0496c314829f8d80fe2b77257f0
victor rene castañon <vrcastanon@gmail.com>
2021-07-22 18:01:44 -0500

    DDL y DML Perfilamiento usuarios

--- EjecutarScriptMicroblow.sql
+++ EjecutarScriptMicroblow.sql
@@ -1,6 +1,10 @@
 SET NOCOUNT ON
 GO
 :On Error exit
-:r C:\Users\Servidor\Documents\Rene\Scripts\Microblow\DML_Script_1.sql
+:r C:\Users\Servidor\Documents\Rene\Scripts\Microblow\DDL_Roles.sql
+:r C:\Users\Servidor\Documents\Rene\Scripts\Microblow\DDL_Users.sql
+:r C:\Users\Servidor\Documents\Rene\Scripts\Microblow\DML_CFDI.sql
+:r C:\Users\Servidor\Documents\Rene\Scripts\Microblow\DML_InsertaRoles.sql
+:r C:\Users\Servidor\Documents\Rene\Scripts\Microblow\DML_InsertaUsuarios.sql
 PRINT 'Script Ejecutado correctamente'
 GO
