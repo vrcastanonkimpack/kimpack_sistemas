d1e4ac65e461b431b57076bf1f895f58159db240
victor rene castañon <vrcastanon@kimpack.com>
2021-07-22 14:22:59 -0500

    Se cargan los script para el pefilamiento de usuarios en Microblow

---
+++ EjecutarScriptMicroblow.sql
@@ -0,0 +1,6 @@
+SET NOCOUNT ON
+GO
+:On Error exit
+:r C:\Users\Servidor\Documents\Rene\Scripts\Microblow\DML_Script_1.sql
+PRINT 'Script Ejecutado correctamente'
+GO
---
+++ Microblow/DML_CFDI.sql
@@ -0,0 +1,5 @@
+CREATE proc [dbo].[spCFDI]  as
+begin
+Select CLAVE+'-'+DESCRIPCION
+FROM [KimpackERP].[dbo].[USO_CFDI]
+end
