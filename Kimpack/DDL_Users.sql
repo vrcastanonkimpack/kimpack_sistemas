cc729eb1bdafa3c4b609b9078e8292442e8615a1
JocelynArgueta <81652961+JocelynArgueta@users.noreply.github.com>
2021-07-22 13:57:42 -0500

    Create DDL_Users.sql

---
+++ Kimpack/DDL_Users.sql
@@ -0,0 +1,31 @@
+USE [MicroblowERP]
+GO
+
+/****** Object:  Table [dbo].[USERS]    Script Date: 07/22/2021 11:53:03 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[USERS](
+	[user_id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](70) NULL,
+	[password] [varchar](70) NULL,
+	[enabled] [tinyint] NULL,
+	[nivel] [int] NULL,
+	[act] [int] NULL,
+ CONSTRAINT [PK_USERS] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
