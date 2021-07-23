a3a12dcd32c73b38905d19e1af11b471cb287cfa
victor rene castañon <vrcastanon@gmail.com>
2021-07-22 21:38:30 -0500

    prueba de ejecución para DDL con cabecera

--- Microblow/DML_CFDI.sql
+++ Microblow/DML_CFDI.sql
@@ -1,3 +1,12 @@
+USE [MicroblowERP]
+GO
+
+/****** Object:  StoredProcedure [dbo].[spCancelaBodega]    Script Date: 07/22/2021 21:35:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE proc [dbo].[spCFDI]  as
 begin
 Select CLAVE+'-'+DESCRIPCION
