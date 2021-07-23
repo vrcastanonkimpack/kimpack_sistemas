5a8f56132a6f2b8e5fb0ffffe403c864d6885cf2
victor rene castañon <vrcastanon@gmail.com>
2021-07-22 20:13:11 -0500

    ejecutar DML_InsertaRoles.sql y DML_InsertaUsuarios.sql por error en DML_CFDI.sql

--- EjecutarScriptMicroblow.sql
+++ EjecutarScriptMicroblow.sql
@@ -1,10 +1,8 @@
 SET NOCOUNT ON
 GO
 :On Error exit
-:r C:\Users\ServidorP\Documents\Scripts\Microblow\DDL_Roles.sql
-:r C:\Users\ServidorP\Documents\Scripts\Microblow\DDL_Users.sql
-:r C:\Users\ServidorP\Documents\Scripts\Microblow\DML_CFDI.sql
 :r C:\Users\ServidorP\Documents\Scripts\Microblow\DML_InsertaRoles.sql
+PRINT 'Script Ejecutado correctamente 2'
 :r C:\Users\ServidorP\Documents\Scripts\Microblow\DML_InsertaUsuarios.sql
-PRINT 'Script Ejecutado correctamente'
+PRINT 'Script Ejecutado correctamente 3'
 GO
