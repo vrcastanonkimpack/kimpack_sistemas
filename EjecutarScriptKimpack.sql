4f46318f0f8b2bf0db24d148c4ae2e14885705d2
JUAN ORTEGA <84634040+SistemasKimpack@users.noreply.github.com>
2021-08-16 14:53:21 -0500

    Update EjecutarScriptKimpack.sql

--- EjecutarScriptKimpack.sql
+++ EjecutarScriptKimpack.sql
@@ -1,6 +1,6 @@
 SET NOCOUNT ON
 GO
 :On Error exit
-:r C:\Users\ServidorP\Documents\Scripts\Kimpack\DML_cambioEliminaProd.sql
+:r C:\Users\ServidorP\Documents\Scripts\Kimpack\DML_cancelaQlAlmacen.sql
 PRINT 'Script Ejecutado correctamente'
 GO
