282a97bb0aca241d04709898538bec307a90f568
JocelynArgueta <81652961+JocelynArgueta@users.noreply.github.com>
2021-08-20 13:27:37 -0500

    Update EjecutarScriptKimpack.sql

--- EjecutarScriptKimpack.sql
+++ EjecutarScriptKimpack.sql
@@ -1,6 +1,6 @@
 SET NOCOUNT ON
 GO
 :On Error exit
-:r C:\Users\ServidorP\Documents\Scripts\Kimpack\DML_cancelaQlAlmacen.sql
+:r C:\Users\ServidorP\Documents\Scripts\Kimpack\DDL_AsignacionCodigosEmpaques.sql
 PRINT 'Script Ejecutado correctamente'
 GO
