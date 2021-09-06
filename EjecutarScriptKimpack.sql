e7b73ba40aebbf819eba47d9b50dce6d5e9d6c35
JocelynArgueta <81652961+JocelynArgueta@users.noreply.github.com>
2021-09-06 11:01:39 -0500

    Update EjecutarScriptKimpack.sql

--- EjecutarScriptKimpack.sql
+++ EjecutarScriptKimpack.sql
@@ -1,6 +1,6 @@
 SET NOCOUNT ON
 GO
 :On Error exit
-:r C:\Users\ServidorP\Documents\Scripts\Kimpack\DDL_AsignacionCodigosEmpaques.sql
+:r C:\Users\ServidorP\Documents\Scripts\Kimpack\DDL_COMPUESTO_MATERIAL_PESOS.sql
 PRINT 'Script Ejecutado correctamente'
 GO
